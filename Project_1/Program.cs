b564c6c5f60de63f0bb2e0f0ec726cd64e819477
Oleg_bul <kokseg63@gmail.com>
2022-09-22 23:03:14 +0400

    Project_1 complete

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int NumberOne(int num, int NumberOfFirstNumbers)
+{
+    while (num / Math.Pow(10, NumberOfFirstNumbers) > 1)
+    {
+        num /= 10;
+    }
+    return num;
+}
+
+int number = int.Parse(Console.ReadLine());
+if (number < 0)
+    number *= -1;
+if (number > 99)
+{
+    int x = number % 100;
+    int y = NumberOne(number, 2);
+    if (x / 10 == y % 10 && x % 10 == y / 10)
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является полиндромом");
+    }
+}
+else if (number > 9)
+{
+    int x = number % 10;
+    int y = NumberOne(number, 1);
+}
+else
+{
+    Console.WriteLine("Введите число больше 9");
+}
